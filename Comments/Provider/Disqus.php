cba69b7426dc622c8fed3adeddaa69fd218f670c
Jérôme Vieilledent <lolautruche@gmail.com>
2013-09-13 18:00:20 +0200

    First implementation of Disqus provider

--- Comments/Provider/Disqus.php
+++ Comments/Provider/Disqus.php
@@ -12,14 +12,48 @@ namespace EzSystems\CommentsBundle\Comments\Provider;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use EzSystems\CommentsBundle\Comments\ProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Templating\EngineInterface;
 
 class Disqus implements ProviderInterface
 {
-    protected $disqusShortName;
+    /**
+     * Disqus forum's shortname.
+     *
+     * @var string
+     */
+    protected $shortName;
 
-    public function __construct( $disqusShortname )
+    /**
+     * Template to use by default to render Disqus thread.
+     *
+     * @var string
+     */
+    private $defaultTemplate;
+
+    /**
+     * @var \Symfony\Component\Templating\EngineInterface
+     */
+    private $templateEngine;
+
+    public function __construct( EngineInterface $templateEngine, $defaultTemplate )
     {
-        $this->disqusShortName = $disqusShortname;
+        $this->templateEngine = $templateEngine;
+        $this->defaultTemplate = $defaultTemplate;
+    }
+
+    public function setShortName( $shortName )
+    {
+        $this->shortName = $shortName;
+    }
+
+    public function getDefaultTemplate()
+    {
+        return $this->defaultTemplate;
+    }
+
+    protected function getTemplateEngine()
+    {
+        return $this->templateEngine;
     }
 
     /**
@@ -33,12 +67,16 @@ class Disqus implements ProviderInterface
      */
     public function render( Request $request, array $options = array() )
     {
-        // TODO: Implement render() method.
+        return $this->doRender(
+            $options + array(
+                'identifier' => $request->getPathInfo()
+            )
+        );
     }
 
     /**
      * Renders the comments list for a given content.
-     * Comment form might also be included
+     * Comment form might also be included.
      *
      * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo $contentInfo
      * @param \Symfony\Component\HttpFoundation\Request $request
@@ -48,6 +86,29 @@ class Disqus implements ProviderInterface
      */
     public function renderForContent( ContentInfo $contentInfo, Request $request, array $options = array() )
     {
-        // TODO: Implement renderForContent() method.
+        $siteAccessName = $request->attributes->has( 'siteaccess' ) ? $request->attributes->get( 'siteaccess' )->name : 'default';
+
+        return $this->doRender(
+            $options + array(
+                'identifier' => $siteAccessName . '/' . $contentInfo->id,
+                // TODO: Use translated name
+                'title' => $contentInfo->name,
+            )
+        );
+    }
+
+    /**
+     * Renders the template with provided options.
+     * "template" option allows to override the default template for rendering.
+     *
+     * @param array $options
+     * @return string
+     */
+    protected function doRender( array $options )
+    {
+        $template = isset( $options['template'] ) ? $options['template'] : $this->getDefaultTemplate();
+        unset( $options['template'] );
+
+        return $this->getTemplateEngine()->render( $template, $options );
     }
 }
