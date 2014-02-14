b5dd793afc21f040a3cf5fc20c07af5c6ebc6168
Bertrand Dunogier <bd@ez.no>
2014-02-14 10:55:30 +0100

    EZP-22210: Removed useless settings

--- ezpublish_legacy/ezcommentsbundle/settings/admininterface.ini.append.php
+++ ezpublish_legacy/ezcommentsbundle/settings/admininterface.ini.append.php
@@ -9,11 +9,3 @@ Description=
 NavigationPartName=ezcontentnavigationpart
 HeaderTemplate=commentsbundle_header.tpl
 Template=commentsbundle.tpl
-
-[ContentEditSettings]
-# Templates to include in the left menu of content/edit
-# for instance
-# LeftMenuParts[]=content/parts/advanced.tpl
-# will include "design:content/parts/advanced.tpl"
-# from design/admin/templates/content/edit_menu.tpl
-LeftMenuParts[]
